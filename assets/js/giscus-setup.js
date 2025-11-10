c8eb58d92106fc908ad6f679da320adb6549513a
pcallec <pcallec@outlook.com>
2025-11-10 17:32:46 +0000

    Deploying to gh-pages from @ TranslationalNeuroimagingLab/translationalneuroimaginglab.github.io@8bd4bff9884ff698ef6f2bfa44dc4188d10b125c 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "TranslationalNeuroimagingLab/millerlab.github.io",
+    "data-repo": "TranslationalNeuroimagingLab/translationalneuroimaginglab.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
