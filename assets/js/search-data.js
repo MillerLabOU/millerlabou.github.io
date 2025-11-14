8bed12f08f1fc9e71cd3e48a4180811b1f87d0a6
pcallec <pcallec@outlook.com>
2025-11-14 23:27:34 +0000

    Deploying to gh-pages from @ MillerLabOU/millerlabou.github.io@713b0e3c2f0e117539540038922e84b870131d7c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,9 +23,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+        },{id: "nav-research-projects",
+          title: "Research Projects",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -456,14 +456,14 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-longitudinal-studies-of-brain-tumor-hemodynamics-and-blood-vessel-morphology",
+          title: 'Longitudinal studies of brain tumor hemodynamics and blood vessel morphology',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-detecting-complications-during-neurosurgery-with-machine-learning",
+          title: 'Detecting complications during neurosurgery with machine learning',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
