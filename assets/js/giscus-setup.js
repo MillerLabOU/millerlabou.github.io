90db863da81543950f91dced9c7d8b5f65a844df
pcallec <pcallec@outlook.com>
2025-11-14 21:30:20 +0000

    Deploying to gh-pages from @ MillerLabOU/millerlabou.github.io@9e982be744338a799003f0c4d8b61c9e4e534ab0 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "TranslationalNeuroimagingLab/translationalneuroimaginglab.github.io",
+    "data-repo": "MillerLabOU/millerlabou.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
